cd1bf9b58d6594edbba072440921978072640578
Joao Henrique <joaohenriquetdj@gmail.com>
2020-09-07 15:32:51 -0300

    Exception delete user

--- src/main/java/com/example/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.course.services.exceptions.DatabaseException;
 import com.example.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -22,4 +23,15 @@ public class ResourceExceptionHandler  {
 		
 		return ResponseEntity.status(status).body(standardError);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException ex,HttpServletRequest req){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError standardError = new StandardError(Instant.now(),status.value(),error,ex.getMessage(),req.getRequestURI());
+		
+		return ResponseEntity.status(status).body(standardError);
+	}
+	
+	
 }
